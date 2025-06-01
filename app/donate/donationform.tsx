62d5ea6db0b13eb494bc315f88f7c7dee349c15b
Tricky <patrickannang23@gmail.com>
2025-06-01 09:03:25 +0000

    fix zod error

--- app/donate/donationform.tsx
+++ app/donate/donationform.tsx
@@ -18,13 +18,24 @@ export function DonationForm() {
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div>
           <input
-            {...register(firstName)}
+            {...register("firstName", { required: true })}
             type="text"
             placeholder="First Name"
             className="px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#228b22] focus:border-[#228b22] transition-colors"
           />
           {errors.firstName && (
             <p className="text-red-500">{errors.firstName.message}</p>
+          )}
+        </div>
+        <div>
+          <input
+            {...register("lastName", { required: true })}
+            type="text"
+            placeholder="Last Name"
+            className="px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-[#228b22] focus:border-[#228b22] transition-colors"
+          />
+          {errors.lastName && (
+            <p className="text-red-500">{errors.lastName.message}</p>
           )}
         </div>
       </div>
